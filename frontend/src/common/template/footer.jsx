724008952514db816a7040ce71633077446e9645
vrsfilho <38162783+vrsfilho@users.noreply.github.com>
2019-02-27 13:02:42 -0300

    Add files via upload

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'>
+        <strong>
+            Copyright &copy; 2019
+            <a href='http://vrsfilho.com'  target='_blank'> Vrsfilho</a>.
+        </strong>
+    </footer>
+)
